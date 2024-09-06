e76e47dbe63af446fd884dc638c43d9cc0b74d8c
SCC-ONS-Backoffice <backoffice-sd@ssc-ons.nl>
2024-09-06 13:26:23 +0200

    Update START.bat

--- START.bat
+++ START.bat
@@ -1,18 +1,28 @@
 @echo off
 
-REM Test internet connection
-ping -n 1 google.com >nul 2>&1
+REM Test internet connection by pinging a reliable IP (Google DNS)
+ping -n 1 8.8.8.8 >nul 2>&1
 if %errorlevel% neq 0 (
     echo Geen internetverbinding gevonden. Controleer uw netwerkverbinding en probeer het opnieuw.
+    pause
     exit /b 1
 )
 
 echo Internetverbinding gedetecteerd.
 
+REM Download PowerShell script
 echo Downloading PowerShell script...
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SCC-ONS-Backoffice/Autopilot-join-SSC-ONS/main/Tenant-join.ps1' -OutFile 'Tenant-join.ps1'"
+powershell -Command "try {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SCC-ONS-Backoffice/Autopilot-join-SSC-ONS/main/Tenant-join.ps1' -OutFile 'Tenant-join.ps1'} catch { Write-Host 'Fout bij het downloaden van het script.'; exit 1 }"
 
+if not exist "Tenant-join.ps1" (
+    echo Het PowerShell-script kon niet worden gedownload.
+    pause
+    exit /b 1
+)
+
+REM Run PowerShell script
 echo Running PowerShell script...
 powershell -ExecutionPolicy Bypass -File .\Tenant-join.ps1
 
 echo Script execution complete.
+pause
