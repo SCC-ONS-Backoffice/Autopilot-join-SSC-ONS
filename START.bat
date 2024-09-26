cf4f47e5adaef868df0dcfce52ea3a6898bf9713
SCC-ONS-Backoffice <backoffice-sd@ssc-ons.nl>
2024-09-26 18:47:16 +0200

    Update START.bat

--- START.bat
+++ START.bat
@@ -1,6 +1,6 @@
 @echo off
 
-REM Test internet connection by pinging a reliable IP (Google DNS)
+REM Test internetverbinding door te pingen naar een betrouwbaar IP (Google DNS)
 ping -n 1 8.8.8.8 >nul 2>&1
 if %errorlevel% neq 0 (
     echo Geen internetverbinding gevonden. Controleer uw netwerkverbinding en probeer het opnieuw.
@@ -10,9 +10,12 @@ if %errorlevel% neq 0 (
 
 echo Internetverbinding gedetecteerd.
 
-REM Download PowerShell script
-echo Downloading PowerShell script...
-powershell -Command "try {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SCC-ONS-Backoffice/Autopilot-join-SSC-ONS/main/Tenant-join.ps1' -OutFile 'Tenant-join.ps1'} catch { Write-Host 'Fout bij het downloaden van het script.'; exit 1 }"
+REM Verwijder bestaand PowerShell-script indien aanwezig
+if exist "Tenant-join.ps1" del /f /q "Tenant-join.ps1"
+
+REM Download PowerShell-script
+echo Bezig met downloaden van PowerShell-script...
+powershell -Command "try {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SCC-ONS-Backoffice/Autopilot-join-SSC-ONS/main/Tenant-join.ps1' -Headers @{'Cache-Control'='no-cache'; 'Pragma'='no-cache'} -OutFile 'Tenant-join.ps1'} catch { Write-Host 'Fout bij het downloaden van het script.'; exit 1 }"
 
 if not exist "Tenant-join.ps1" (
     echo Het PowerShell-script kon niet worden gedownload.
@@ -20,9 +23,9 @@ if not exist "Tenant-join.ps1" (
     exit /b 1
 )
 
-REM Run PowerShell script
-echo Running PowerShell script...
+REM Voer PowerShell-script uit
+echo Bezig met uitvoeren van PowerShell-script...
 powershell -ExecutionPolicy Bypass -File .\Tenant-join.ps1
 
-echo Script execution complete.
+echo Scriptuitvoering voltooid.
 pause
